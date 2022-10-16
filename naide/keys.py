5240352e00fa1049d9adce7143799282c5c47fb5
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-16 23:57:23 +0200

    Create keys.py

---
+++ naide/keys.py
@@ -0,0 +1,6 @@
+from ..menubar import Save, selectall, autosaveevent
+def process_window(window, txt):
+  window.bind('<Control-s>', Save)
+  window.bind('<Control-a>', selectall)
+  txt.bind('<Key>', autosaveevent)
+  return window, txt

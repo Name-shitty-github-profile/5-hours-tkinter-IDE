1eee97c6eebd105867844301fcc1a5fdc6b7238c
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-08-29 19:46:50 -0400

    Update utils.py

--- naide/utils.py
+++ naide/utils.py
@@ -1,3 +1,9 @@
 from tkinter import messagebox
-def popup(title: str, desc: str):
-  messagebox.showinfo(title, desc)
+def popup(*args, **qwargs):
+  try:
+    messagebox.showinfo(*args, **qwargs)
+  except Exception as e:
+    try:
+      messagebox.showinfo("ERROR CATCHED", f"Error : {e}\nArgs : {args}\nQwargs : {qwargs}")
+    except Exception as e2:
+      print(f"ERROR CATCHED\nError : {e2}\nOriginal Error : {e}\nArgs : {args}\nQwargs : {qwargs}")

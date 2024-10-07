bc2d0ca349580cfb74af55c951e4eb361ec501e1
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-10-07 10:55:15 -0400

    Update keys.py

--- naide/keys.py
+++ naide/keys.py
@@ -1,6 +1,6 @@
 from ..menubar import Save, selectall, autosaveevent
 def process_window(window, txt):
-  window.bind('<Control-s>', Save)
-  window.bind('<Control-a>', selectall)
+  for k, v in ({"<Control-s>": Save, "<Control-a>": selectall):
+    window.bind(k, v)
   txt.bind('<Key>', autosaveevent)
   return window, txt
